1d944385926b0686159e1977ac9eb22a266b0d56
Balaji9817 <balajisiva1998@gmail.com>
2025-08-24 22:36:49 +0530

    up2

--- TableTDD/src/test/java/Test/Code_TDD_RW.java
+++ TableTDD/src/test/java/Test/Code_TDD_RW.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -30,7 +31,7 @@ public class Code_TDD_RW {
 		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));	 	
 	}
 	
-	@Test
+	@Test(priority=1)
 	public void testDataProcessing() {
 		List<WebElement> ele1 = driver.findElements(By.cssSelector("div[class='card-body']"));
 		for(WebElement ele : ele1) {
@@ -46,11 +47,62 @@ public class Code_TDD_RW {
 		
 	}
 	
-	@AfterClass
-	public void tearDownAfterClass() throws Exception {
-		// Code to clean up after all tests in this class have run
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		driver.quit();
+	@Test(dataProvider="check", priority=2)
+	public void Wdtable(String name,String Lname,String age,String gmail,String sal,String dept )
+	{
+		wait= new WebDriverWait(driver, Duration.ofSeconds(50));
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[class='element-list collapse show'] li[id='item-3']")));
+		driver.findElement(By.cssSelector("div[class='element-list collapse show'] li[id='item-3']")).click();
+		
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("h1[class='text-center']")));
+		driver.findElement(By.cssSelector("button[id='addNewRecordButton']")).click();
+		
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[class='modal-content']")));
+		
+		List<WebElement> eledata=driver.findElements(By.cssSelector("input[class=' mr-sm-2 form-control']"));
+		String str[]= {name, Lname, age, sal, dept};
+		
+		for(int i=0;i<eledata.size();i++)
+		{
+			eledata.get(i).click();
+			eledata.get(i).sendKeys(str[i]);
+		}
+		driver.findElement(By.cssSelector("input[class='mr-sm-2 form-control']")).click();
+		driver.findElement(By.cssSelector("input[class='mr-sm-2 form-control']")).sendKeys(gmail);
+		
+		driver.findElement(By.cssSelector("button[id=submit]")).click();
+			        
 	}
+	
+	@DataProvider(name="check")
+	public Object[][] getdata()
+	{
+		Object[][] data=new Object[2][6];
+		
+		data[0][0]="Sidth";
+		data[0][1]="vick";
+		data[0][2]="36";
+		data[0][3]="sidth@ggmil.com";
+		data[0][4]="234567";
+		data[0][5]="Mech";
+		
+		data[1][0]="keerthi";
+		data[1][1]="daugth";
+		data[1][2]="56";
+		data[1][3]="keerti@ggmil.com";
+		data[1][4]="56874";
+		data[1][5]="ECE";
+		
+		return data;
+	}
+	
+	
+	
+//	@AfterClass
+//	public void tearDownAfterClass() throws Exception {
+//		// Code to clean up after all tests in this class have run
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+//		driver.quit();
+//	}
 
 }
